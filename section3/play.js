a41da42ae650f36f54979d3305bacd969be0be1b
David <corbodiei@gmail.com>
2020-07-07 14:32:21 -0500

    finished section 3

---
+++ section3/play.js
@@ -0,0 +1,16 @@
+let name = "max";
+let age = 29;
+let hasHobbies = true;
+
+function summarizeUser(userName, userAge, userHasHobbies) {
+  return (
+    "Name is " +
+    userName +
+    ", age is " +
+    userAge +
+    " and the user has hobbies: " +
+    userHasHobbies
+  );
+}
+
+console.log(summarizeUser(name, age, hasHobbies));
